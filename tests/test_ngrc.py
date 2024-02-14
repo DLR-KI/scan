646acb4f04078752ade892f89b38ae093d89681e
Koeglmayr, Daniel <daniel.koeglmayr@dlr.de>
2024-02-14 12:08:26 +0100

    Add first ngrc pytest

---
+++ tests/test_ngrc.py
@@ -0,0 +1,31 @@
+""" Tests if the scan.esn module works as it should """
+
+import copy
+
+import numpy as np
+import pytest
+
+import scan
+from tests.test_base import TestScanBase, assert_array_almost_equal, assert_array_equal
+
+
+# Use print(np.array_repr(np_array, max_line_width=120, precision=18)) in the debugger to easily get the copy-pastable
+# representation of a numpy array.
+class TestNGRC(TestScanBase):
+    def setUp(self):
+        self.set_seed()
+        self.esn = scan.ESN()
+        np.set_printoptions(linewidth=120, precision=20)
+
+    def tearDown(self):
+        del self.esn
+        np.random.seed(None)
+        np.set_printoptions()
+
+    def test_initiate_ngrc_base_class_instance(self):
+        ngrc = scan.ngrc.NG_RC(
+            k=2,
+            s=1
+        )
+        
+        assert isinstance(ngrc,scan.ngrc.NG_RC)
